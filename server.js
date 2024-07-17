4788c7a1a79c16888768944b783e0ef00fd219a0
Krishnendu Mitra <150600460+kidKrishkode@users.noreply.github.com>
2024-07-17 21:03:10 +0530

    Version 1.0

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
 const {spawn} = require('child_process');
+const exec = require('child_process').exec;
+const ejs = require('ejs');
+const jsonfile = require('jsonfile');
+require('./public/App.test.js');
 require('dotenv').config();
 
 const app = express();
 let server = http.createServer(app);
 const PORT = process.env.PORT || 5500;
 const AppName = "Streamline Diagnosis";
+let web;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -29,13 +34,15 @@ app.get('/diagnosis', (req, res) => {
 });
 
 app.get('/diabetes', (req, res) => {
-    res.status(200).render('diabetes');
+    const layout = undefined;
+    const relatedDiagnosis = web.getRelatedDiagnosis('Diabetes').toString();
+    res.status(200).render('diabetes',{layout, relatedDiagnosis});
 });
 
 app.get('/diabetes/predict', async (req, res) => {
     try{
         const name = req.query.name;
-        const age = getAge(req.query.age);
+        const age = web.getAge(req.query.age);
         const bp = req.query.bp * 1;
         const glu = req.query.glu * 1;
         const fru = req.query.fru * 1;
@@ -44,8 +51,16 @@ app.get('/diabetes/predict', async (req, res) => {
         const wl = req.query.wl * 1;
         const listOfInput = [age, bp, glu, fru, blv, slh, wl];
         if(listOfInput.length > 0){
-            await callPythonProcess(listOfInput, 'diabetes').then(result => {
-                res.status(200).render('diabetes',{name, age, bp, glu, fru, blv, slh, wl, result});
+            let new_fru = fru==0?'No':'Yes';
+            let new_blv = blv==0?'No':'Yes';
+            let new_slh = slh==0?'No':'Yes';
+            let new_wl = wl==0?'No':'Yes';
+            let feature = web.featureLayout(['Blood Pressure', 'Glucose Level', 'Frequent Urination', 'Blurred Vision', 'Slow Healing', 'Weight Loss'],[bp, glu, new_fru, new_blv, new_slh, new_wl],['mmHg','mg/dL',null,null,null,null]);
+            await callPythonProcess(listOfInput, 'diabetes').then(results => {
+                const result = web.targetLayout(['Result'],[results.value==0?'Negative (No Diabetes Detected)':'Positive (Diabetes Detected)']);
+                ejs.renderFile('./views/output.ejs',{name, age, feature, result}).then(layout => {
+                    res.status(200).render('diabetes',{layout});
+                });
             }).catch(error => {
                 console.error('Error:', error.message);
             });
@@ -55,8 +70,66 @@ app.get('/diabetes/predict', async (req, res) => {
     }
 });
 
-function getAge(time){
+function WEB(port){
+    this.active = true;
+    this.port = port;
+    this.filename = path.basename(__filename);
+}
+
+WEB.prototype.getAge = function(time){
     return (new Date().getFullYear()) - ((time[0]*1000)+(time[1]*100)+(time[2]*10)+(time[3]*1));
+}
+
+WEB.prototype.featureLayout = function(head, value, unit){
+    try{
+        let heading = '';
+        let input = '';
+        for(let i=0; i<head.length; i++){
+            if(i==0){
+                heading += '<p>';
+                input += '<p>';
+            }
+            heading += `<span>${head[i]}: </span><br>`;
+            input += `${value[i]} ${unit[i]==null?'':unit[i]}<br>`;
+            if(i==head.length-1){
+                heading += '</p>';
+                input += '</p>';
+            }
+        }
+        let layout = heading + input;
+        return layout;
+    }catch(e){
+        return null;
+    }
+}
+
+WEB.prototype.targetLayout = function(head,statment){
+    let layout='';
+    for(let i=0; i<head.length; i++){
+        if(i==0){
+            layout += '<p class="wd-lg">';
+        }
+        layout += `<span>${head[i]}: </span> ${statment[i]}<br>`;
+        if(i==head.length-1){
+            layout += '</p>';
+        }
+    }
+    return layout;
+}
+
+WEB.prototype.getRelatedDiagnosis = function(relationName){
+    const data = jsonfile.readFileSync('./public/manifest.json');
+    const relatedDiagnoses = [];
+    data['Related-diagnosis'].forEach(diagnosis => {
+        if(diagnosis.related.includes(relationName)){
+            relatedDiagnoses.push(diagnosis);
+        }
+    });
+    let layout='';
+    for(let i=0; i<relatedDiagnoses.length; i++){
+        layout += `<li onclick="route('${relatedDiagnoses[i].link}');">${relatedDiagnoses[i].name}</li>`;
+    }
+    return layout;
 }
 
 function callPythonProcess(list, functionValue){
@@ -95,7 +168,8 @@ app.get('*', (req, res) => {
 
 server.listen(PORT, (err) => {
     if(err) console.log("Oops an error occure:  "+err);
-    console.log(`Compiled successfully!\n\nYou can now view ./${path.basename(__filename)} in the browser.`);
+    web = new WEB(PORT);
+    console.log(`Compiled successfully!\n\nYou can now view \x1b[33m./${path.basename(__filename)}\x1b[0m in the browser.`);
     console.info(`\thttp://localhost:${PORT}`);
-    console.log("\nNode web compiled!\n");
+    console.log("\n\x1b[32mNode web compiled!\x1b[0m \n");
 });
