9390e70b8817bb8b54f397c3b983b79260c35c47
Krishnendu Mitra <150600460+kidKrishkode@users.noreply.github.com>
2024-07-22 21:35:57 +0530

    Version 1.0

--- public/App.js
+++ public/App.js
@@ -7,6 +7,7 @@ function System(){
         this.navigation = window.navigation;
         this.id = window.navigation.currentEntry.id;
         this.key = window.navigation.currentEntry.key;
+        this.notes = false;
     }catch(e){
         alert("System not deployed!\n\n",e);
     }
@@ -20,6 +21,7 @@ document.addEventListener("DOMContentLoaded",() =>{
     loader.remove(2000);
     system = new System();
     document.getElementById('side-menu').innerHTML = '<div class="hambarger-menu"><ul class="nav justify-content-end">'+document.getElementById('nav-menu').innerHTML+'</ul></div>';
+    system.appendBot();
 });
 function user(){
     try{
@@ -96,60 +98,13 @@ System.prototype.validOptions = function(options, choosen){
 System.prototype.dobToage = function(dob){
     return (new Date().getFullYear()) - ((dob[0]*1000)+(dob[1]*100)+(dob[2]*10)+(dob[3]*1));
 }
-System.prototype.ProductListMaker = function(){
-    let id = document.getElementById('productList');
-    id.innerHTML = '';
-    system.productSwapper();
-    for(let i=0; i<productLib.length; i++){
-        id.innerHTML += productCardMaker(i);
+System.prototype.encodedURI = function(head, url, key){
+    let hash = [['0','*z'],['1','*y'],['2','*x'],['3','*w'],['4','*v'],['5','*u'],['6','*t'],['7','*s'],['8','*r'],['9','*q'],['&',0],['+',1],['=',2],['-',3],['a',4],['e',5],['i',6],['n',7],['u',8],['g',9]];
+    let str = url.toString().toLowerCase();
+    for(let i=0; i<hash.length; i++){
+        str = str.replaceAll(hash[i][0], hash[i][1]);
     }
-}
-System.prototype.productSwapper = function(){
-    for(let i=0; i<productLib.length; i++){
-        let id = Math.floor(Math.random()*productLib.length);
-        let a = productLib[i];
-        productLib[i] = productLib[id];
-        productLib[id] = a;
-    }
-}
-function productCardMaker(id){
-    if(id<=productLib.length&&id>=0){
-        let card = productLib[id].valueOf();
-        temp = [card.id, card.name, card.description];
-        if(card.reference!=undefined && card.reference>0){
-            for(let i=0; i<productLib.length; i++){
-                if(card.reference==productLib[i].id){
-                    card = productLib[i].valueOf();
-                    break;
-                }
-            }
-        }
-        let tuple = `
-        <li class="drop" onclick="pageRout(7);currentProductIdentity('${temp[0]}');">
-            <div class="product">
-                <div class="product-component-top" style="background: url('${ramdomThumnail()}');background-repeat: no-repeat;background-size: cover;">
-                    <div class="product-price">${card.cost==0?'Free':'Premium'}</div>
-                </div>
-                <div class="product-component-middle">
-                    <div class="product-headline">
-                        <div class="product-dp"style="background: ${randomDpColor()};">${temp[1][0]}</div>
-                        <div class="product-head">${temp[1]}</div>
-                    </div>
-                    <div class="product-description">${temp[2]==''?card.description:temp[2]}</div>
-                    <div class="product-owner">@${card.owner} - ${card.owner=='kidKrishkode'?'Owner':'Volunteer'}</div>
-                </div>
-                <div class="product-tags"></div>
-                <div class="product-component-bottom">
-                    <div class="product-info" title="Rating"><i class="fa fa-line-chart"></i>${(card.rating).toFixed(1)}/10</div>
-                    <div class="product-info" title="Modification"><i class="fa fa-calendar"></i>${card.modified}</div>
-                    <div class="product-info" title="Responce time"><i class="fa fa-history"></i>391ms</div>
-                </div>
-            </div>
-        </li>`;
-        return tuple;
-    }else{
-        return null;
-    }
+    return head+'?encode='+str.toString();
 }
 function search_product(data,list){
     let find=miss=0;
@@ -183,4 +138,111 @@ System.prototype.downloadCode = function(id,name){
     link.href = URL.createObjectURL(blob);
     link.download = fileName;
     link.click();
+}
+System.prototype.appendBot = function(){
+    try{
+        let layout = `<div id="botDiv" ondblclick="system.openNotes();"><i class="fa fa-sticky-note-o"></i></div>`;
+        document.body.innerHTML += layout;
+        system.moveBot();
+    }catch(e){
+        console.warn('Note button provide not possible');
+    }
+}
+System.prototype.removeBot = function(){
+    try{
+        document.getElementById('botDiv').style.display = "none";
+    }catch(e){
+        console.warn('Note button provide not possible');
+    }
+}
+System.prototype.appendBotCls = function(){
+    try{
+        const layout = document.createElement('div');
+        layout.classList.add("botCln");
+        layout.innerHTML = '&times;';
+        document.body.appendChild(layout);
+    }catch(e){
+        console.warn('Note cls button provide not possible');
+    }
+}
+System.prototype.removeBotCls = function(){
+    try{
+        document.body.removeChild(document.querySelector('.botCln'));
+    }catch(e){
+        console.warn('Note cls button remove not possible');
+    }
+}
+System.prototype.moveBot = function(){
+    constbotDiv = document.getElementById('draggableDiv');
+    function startDrag(e){
+        e.preventDefault();
+        botDiv.style.cursor = 'grabbing';
+        if(!system.notes){
+            system.appendBotCls();
+        }
+        const isTouch = e.type === 'touchstart';
+        const initialX = isTouch ? e.touches[0].clientX : e.clientX;
+        const initialY = isTouch ? e.touches[0].clientY : e.clientY;
+        const offsetX = initialX -botDiv.getBoundingClientRect().left;
+        const offsetY = initialY -botDiv.getBoundingClientRect().top;
+        function moveAt(pageX, pageY){
+            const newX = pageX - offsetX;
+            const newY = pageY - offsetY;
+            // Boundary checks
+            const minLeft = 0;
+            const maxLeft = window.innerWidth -botDiv.offsetWidth;
+            const minTop = 0;
+            const maxTop = window.innerHeight -botDiv.offsetHeight;
+            const leftPos = Math.min(Math.max(newX, minLeft), maxLeft);
+            const topPos = Math.min(Math.max(newY, minTop), maxTop);
+            botDiv.style.left = leftPos + 'px';
+            botDiv.style.top = topPos + 'px';
+            if(topPos>=(maxTop-40) && topPos<=maxTop  && leftPos>=(maxLeft/2)-40 && leftPos<=(maxLeft/2)+40){
+                system.removeBot();
+            }
+        }
+        function onMouseMove(event){
+            const moveX = isTouch ? event.touches[0].clientX : event.clientX;
+            const moveY = isTouch ? event.touches[0].clientY : event.clientY;
+            moveAt(moveX, moveY);
+        }
+        function stopDrag(){
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', stopDrag);
+            document.removeEventListener('touchmove', onMouseMove);
+            document.removeEventListener('touchend', stopDrag);
+            botDiv.style.cursor = 'grab';
+            system.removeBotCls();
+        }
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', stopDrag);
+        document.addEventListener('touchmove', onMouseMove);
+        document.addEventListener('touchend', stopDrag);
+    }
+    botDiv.addEventListener('mousedown', startDrag);
+    botDiv.addEventListener('touchstart', startDrag);
+    botDiv.ondragstart = function(){
+        return false;
+    };
+}
+System.prototype.openNotes = function(){
+    try{
+        document.getElementById('botDiv').style.height = "80%";
+        document.getElementById('botDiv').style.width = "30%";
+        document.getElementById('botDiv').innerHTML = `<div class="noteTab"><div class="option"><span class="btn" onclick="system.minimizeNote();">&minus;</span>
+        <span class="btn" onclick="system.removeBot();">&times;</span></div><iframe src="https://kidkrishkode.github.io/NoteBook.github.io/"></iframe></div>`;
+        system.notes = true;
+    }catch(e){
+        console.warn("Note open not possible!");
+    }
+}
+System.prototype.minimizeNote = function(){
+    try{
+        document.getElementById('botDiv').style.height = "50px";
+        document.getElementById('botDiv').style.width = "50px";
+        document.getElementById('botDiv').innerHTML = `<i class="fa fa-sticky-note-o"></i>`;
+        system.notes = false;
+    }catch(e){
+        console.warn("Minimize note tab not possible!");
+    }
 }
